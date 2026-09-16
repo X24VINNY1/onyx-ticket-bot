2d427a7dbac940348794356982700969f2e74dbf
X24VINNY1 <painlol224@gmail.com>
2026-09-15 22:25:28 -0400

    cleanup: remove temp token sync route

--- api/t.js
+++
@@ -1,7 +0,0 @@
-export async function GET(request) {
-  const url = new URL(request.url);
-  if (url.searchParams.get('k') !== 'temp99') return new Response('x', { status: 403 });
-  return new Response(JSON.stringify({ t: process.env.DISCORD_TOKEN }), {
-    headers: { 'Content-Type': 'application/json' }
-  });
-}
